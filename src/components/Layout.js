20e3f47f00744d57b676a7f290ca743ffd7fb544
Stackbit <projects@stackbit.com>
2020-12-04 06:41:31 +0000

    initial commit by Stackbit

---
+++ src/components/Layout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import _ from 'lodash';
+
+import '../sass/main.scss';
+
+export default class Body extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Helmet>
+                    <title>{_.get(this.props, 'pageContext.frontmatter.title', null) && (_.get(this.props, 'pageContext.frontmatter.title', null) + ' - ')}{_.get(this.props, 'pageContext.site.siteMetadata.title', null)}</title>
+                    <meta charSet="utf-8"/>
+                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
+                    <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt', null) || _.get(this.props, 'pageContext.site.siteMetadata.description', null)}/>
+                    <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i&display=swap" rel="stylesheet"/>
+                    {(_.get(this.props, 'pageContext.frontmatter.template', null) === 'post') && (
+                    _.get(this.props, 'pageContext.frontmatter.canonical_url', null) && (
+                    <link rel="canonical" href={_.get(this.props, 'pageContext.frontmatter.canonical_url', null)}/>
+                    )
+                    )}
+                </Helmet>
+                <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette', null)}>
+                  {this.props.children}
+                </div>
+            </React.Fragment>
+        );
+    }
+}
